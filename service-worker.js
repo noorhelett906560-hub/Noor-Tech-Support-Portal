c628f914a99cb5aed8fbd231bf83aa14b08e8ab2
Noor Mohammad <noor.helett906560@gmail.com>
2026-08-11 16:34:36 +0530

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "helett-software-home-v1";
+
+const FILES_TO_CACHE = [
+  "./software.html",
+  "./site.webmanifest",
+  "./helett-app-192.png",
+  "./helett-app-512.png"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request);
+    })
+  );
+});
